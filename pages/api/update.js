7d56b461241c608242ee5553ff203e1c84999cf5
Joost Schuur <jschuur@jschuur.com>
2022-05-23 17:24:51 +0200

    Revalidate home page after videos update

---
+++ pages/api/update.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    await res.unstable_revalidate('/');
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send('Error revalidating');
+  }
+}
