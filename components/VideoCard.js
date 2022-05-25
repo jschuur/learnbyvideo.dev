f60783afb20fc862fc94db482667ef64d94655d5
Joost Schuur <jschuur@jschuur.com>
2022-05-25 20:57:58 +0200

    Thumbnail switch to medium res

--- components/VideoCard.js
+++ components/VideoCard.js
@@ -3,6 +3,8 @@ import TimeAgo from 'react-timeago';
 import englishStrings from 'react-timeago/lib/language-strings/en';
 import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
 
+import VideoThumbnail from './youtube/VideoThumbnail';
+
 const formatter = buildFormatter(englishStrings);
 
 export default function VideoCard({ video }) {
@@ -14,7 +16,7 @@ export default function VideoCard({ video }) {
       className='bg-slate-100 border-slate-700 flex flex-1 flex-col h-full gap-0'
     >
       <a href={`https://youtube.com/watch?v=${youtubeId}`}>
-        <img src={thumbnail} alt={title} className='w-full' />
+        <VideoThumbnail youtubeId={youtubeId} alt={title} className='w-full' />
       </a>
       <div className='p-2 text-sm flex-grow'>
         <span className='font-semibold'>{channel.channelName}</span>:{' '}
---
+++ components/youtube/VideoThumbnail.js
@@ -0,0 +1,39 @@
+const thumbnailResolutions = {
+  default: {
+    prefix: '',
+    width: 120,
+    height: 90,
+  },
+  medium: {
+    prefix: 'mq',
+    width: 320,
+    height: 180,
+  },
+  high: {
+    prefix: 'hq',
+    width: 480,
+    height: 360,
+  },
+  standard: {
+    prefix: 'sd',
+    width: 640,
+    height: 480,
+  },
+  maxres: {
+    prefix: 'maxres',
+    width: 1280,
+    height: 720,
+  },
+};
+
+export default function VideoThumbnail({ res = 'medium', youtubeId, alt, ...rest }) {
+  return (
+    <img
+      width={thumbnailResolutions[res].width}
+      height={thumbnailResolutions[res].height}
+      src={`https://i.ytimg.com/vi/${youtubeId}/${thumbnailResolutions[res].prefix}default.jpg`}
+      alt={alt}
+      {...rest}
+    />
+  );
+}
