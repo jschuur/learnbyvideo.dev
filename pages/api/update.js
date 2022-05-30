5cf39b701f0f19b472471674ab4f77f5947ecbfd
Joost Schuur <jschuur@jschuur.com>
2022-05-30 13:04:09 +0100

    Redirect to homepage on revalidate

--- pages/api/update.js
+++ pages/api/update.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
   try {
     await res.unstable_revalidate('/');
-    return res.json({ revalidated: true });
+    return res.redirect('/');
   } catch (err) {
     return res.status(500).send('Error revalidating');
   }
