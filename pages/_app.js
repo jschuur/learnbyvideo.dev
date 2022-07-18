5374c3a8985df735455da777b58b2816404e776e
Joost Schuur <jschuur@jschuur.com>
2022-07-18 08:24:31 +0100

    basic Load More pagination button

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 import '../styles/globals.css';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
