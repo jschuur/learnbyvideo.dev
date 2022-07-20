185bb3fb8d4ef15871b2aefe49fd05ede982c871
Joost Schuur <jschuur@jschuur.com>
2022-07-20 17:43:51 +0100

    feat: infinite scrolling trigger on scroll

--- components/VideoGrid.js
+++ components/VideoGrid.js
@@ -1,4 +1,5 @@
 import { flatMap, partition } from 'lodash-es';
+import InfiniteScroll from 'react-infinite-scroller';
 import { useInfiniteQuery } from 'react-query';
 
 import { fetchRecentVideos } from '../lib/request';
@@ -8,22 +9,27 @@ import VideoCard from './VideoCard';
 const sortVideoData = (data) => partition(flatMap(data.pages, 'videos'), { status: 'LIVE' }).flat();
 
 export default function VideoGrid({ initialVideoData }) {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status, error } = useInfiniteQuery(
-    ['recentVideos'],
-    fetchRecentVideos,
-    {
-      initialData: { pages: [initialVideoData] },
-      getNextPageParam: (lastPage) => lastPage.pageInfo.nextOffset,
-      staleTime: Infinity,
-    }
-  );
+  const { data, fetchNextPage, hasNextPage, status, error } = useInfiniteQuery(['recentVideos'], fetchRecentVideos, {
+    initialData: { pages: [initialVideoData] },
+    getNextPageParam: (lastPage) => lastPage.pageInfo.nextOffset,
+    staleTime: Infinity,
+  });
 
   return status === 'loading' ? (
     <div className="mt-4 p-2 text-center border-black border">Loading...</div>
   ) : status === 'error' ? (
     <div className="mt-4 p-2 text-center border-red-600 border">Error: {error.message}</div>
   ) : (
-    <div>
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={fetchNextPage}
+      hasMore={hasNextPage}
+      loader={
+        <div className="mt-4 p-2 text-center border-black border" key={0}>
+          Getting more recent videos...
+        </div>
+      }
+    >
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {sortVideoData(data).map((video) => (
           <div key={video.youtubeId}>
@@ -31,17 +37,6 @@ export default function VideoGrid({ initialVideoData }) {
           </div>
         ))}
       </div>
-      <div
-        onClick={() => fetchNextPage()}
-        onKeyPress={() => fetchNextPage()}
-        disabled={!hasNextPage || isFetchingNextPage}
-        role="button"
-        tabIndex={0}
-        className="mt-4 p-2 text-center border-black border"
-      >
-        {' '}
-        {isFetchingNextPage ? 'Loading more...' : 'Load More'}
-      </div>
-    </div>
+    </InfiniteScroll>
   );
 }
