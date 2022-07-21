59eee04182fbe7c741b50d196d101ba6281ab052
Joost Schuur <jschuur@jschuur.com>
2022-07-21 22:23:09 +0100

    refactor: useRecentVideos hook

--- components/VideoGrid.js
+++ components/VideoGrid.js
@@ -1,35 +1,30 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
 import { flatMap, partition } from 'lodash-es';
 import InfiniteScroll from 'react-infinite-scroller';
 import { SpinnerInfinity } from 'spinners-react';
 
-import { fetchRecentVideos } from '../lib/request';
-
 import VideoCard from './VideoCard';
+
+import useRecentVideos from '../lib/hooks/useRecentVideos';
 
 const sortVideoData = (data) => partition(flatMap(data.pages, 'videos'), { status: 'LIVE' }).flat();
 
+function LoadingSpinner() {
+  return (
+    <div className="mt-4 p-2 flex flex-col justify-center items-center">
+      <SpinnerInfinity size={50} thickness={80} speed={100} color="black" secondaryColor="lightgray" />
+    </div>
+  );
+}
+
 export default function VideoGrid() {
-  const { data, fetchNextPage, hasNextPage, status, error } = useInfiniteQuery(['recentVideos'], fetchRecentVideos, {
-    getNextPageParam: (lastPage) => lastPage.pageInfo.nextOffset,
-    staleTime: Infinity,
-  });
+  const { data, fetchNextPage, hasNextPage, status, error } = useRecentVideos();
 
   return status === 'loading' ? (
     <div className="mt-4 p-2 text-center border-black border">Loading...</div>
   ) : status === 'error' ? (
     <div className="mt-4 p-2 text-center border-red-600 border">Error: {error.message}</div>
   ) : (
-    <InfiniteScroll
-      pageStart={0}
-      loadMore={fetchNextPage}
-      hasMore={hasNextPage}
-      loader={
-        <div className="mt-4 p-2 flex flex-col justify-center items-center" key={0}>
-          <SpinnerInfinity size={50} thickness={80} speed={100} color="black" secondaryColor="lightgray" />
-        </div>
-      }
-    >
+    <InfiniteScroll pageStart={0} loadMore={fetchNextPage} hasMore={hasNextPage} loader={<LoadingSpinner key={0} />}>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {sortVideoData(data).map((video) => (
           <div key={video.youtubeId}>
