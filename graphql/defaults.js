407ae28e63f4948f6e4cdb995d75dc571cb8df47
Joost Schuur <jschuur@jschuur.com>
2022-07-23 17:21:44 +0100

    refactor: use .graphql files for query sharing

---
+++ graphql/defaults.js
@@ -0,0 +1,3 @@
+export default {
+  limit: parseInt(process.env.NEXT_PUBLIC_VIDEO_GRID_COUNT || 80, 10),
+};
---
+++ graphql/queries/index.js
@@ -0,0 +1,4 @@
+import recentVideosQuery from './recentVideos.graphql';
+import siteDataQuery from './siteData.graphql';
+
+export { recentVideosQuery, siteDataQuery };
