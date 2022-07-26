513c4430539c4df09f9cfa3b07bf7261343b9394
Joost Schuur <jschuur@jschuur.com>
2022-07-26 22:43:55 +0100

    refactor: serealise via superjson

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,13 @@ import '../styles/globals.css';
 
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { dehydratedState, ...remainingPageProps } }) {
   const [queryClient] = useState(() => new QueryClient());
-  // eslint-disable-next-line no-eval
-  const deserializedDehydratedState = eval(`(${pageProps.dehydratedState})`);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={deserializedDehydratedState}>
-        <Component {...pageProps} />
+      <Hydrate state={dehydratedState}>
+        <Component {...remainingPageProps} />
       </Hydrate>
     </QueryClientProvider>
   );
