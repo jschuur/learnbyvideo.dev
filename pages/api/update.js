effd42abe06c03faa725257207832d16b242918a
Joost Schuur <jschuur@jschuur.com>
2023-05-04 13:00:31 +0100

    chore: tweak update route error logging

--- pages/api/update.js
+++ pages/api/update.js
@@ -8,6 +8,6 @@ export default async function handler(req, res) {
     await res.revalidate('/');
     return res.redirect('/');
   } catch (err) {
-    return res.status(500).send('Error revalidating');
+    return res.status(500).send(`Error revalidating ${err.message}`);
   }
 }
