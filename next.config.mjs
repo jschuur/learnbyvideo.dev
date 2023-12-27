605a83df846529f772ed80e86f723e1e38826e1f
Joost Schuur <jschuur@jschuur.com>
2023-12-27 16:36:38 +0000

    feat: sentry error tracking

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,41 @@
 import BundleAnalyzer from '@next/bundle-analyzer';
+import { withSentryConfig } from '@sentry/nextjs';
 
 const withBundleAnalyzer = BundleAnalyzer({
-	enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true',
 });
 
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		/* cspell:disable */
-		domains: [
-			'i.ytimg.com',
-			'i1.ytimg.com',
-			'i2.ytimg.com',
-			'i3.ytimg.com',
-			'i4.ytimg.com',
-			'i5.ytimg.com',
-			'i6.ytimg.com',
-			'i7.ytimg.com',
-		],
-		/* cspell:enable */
-	},
+  reactStrictMode: true,
+  images: {
+    /* cspell:disable */
+    domains: [
+      'i.ytimg.com',
+      'i1.ytimg.com',
+      'i2.ytimg.com',
+      'i3.ytimg.com',
+      'i4.ytimg.com',
+      'i5.ytimg.com',
+      'i6.ytimg.com',
+      'i7.ytimg.com',
+    ],
+    /* cspell:enable */
+  },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default withSentryConfig(
+  withBundleAnalyzer(nextConfig),
+  {
+    silent: true,
+    org: 'joost-schuur',
+    project: 'learnbyvideo-dev',
+  },
+  {
+    widenClientFileUpload: true,
+    transpileClientSDK: false,
+    tunnelRoute: '/monitoring',
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  }
+);
