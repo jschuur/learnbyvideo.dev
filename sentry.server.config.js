01195f0ca245916737e89ee77f98e6d1b90c6bf8
Joost Schuur <jschuur@jschuur.com>
2023-12-28 10:09:51 +0000

    feat: conditional sentry init

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,14 +2,16 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: "https://6975a40b7e369f7e832d69e77d8427df@o182865.ingest.sentry.io/4506468576198656",
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
